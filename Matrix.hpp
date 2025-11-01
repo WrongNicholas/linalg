749609381af349b79923c590150ff2f701c01a0d
WrongNicholas <mnwojnar2000@gmail.com>
2025-11-01 03:07:54 -0400

    Add col_at, row_at -- not complete

--- Matrix.hpp
+++ Matrix.hpp
@@ -1,35 +1,68 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
+#include <initializer_list>
 #include <vector>
 #include <span>
 #include <iostream>
 
+// A Matrix represented as a one-dimensional vector, column-major.
+// Can be accessed row-major through helper methods.
+// REQUIRES PROPER INITIALIZATION! (might need to add some sort of throw or check for initialization)
 template <typename T>
 class Matrix {
 private:
-  const int m;
-  const int n;
-  std::vector<T> data;
+  const size_t rows;   // Number of rows in the matrix
+  const size_t cols;   // Number of columns in the matrix
+  std::vector<T> data; // Matrix stored as a one-dimensional vector
 public:
-  Matrix(const int m, const int n);
+  // Constructors
+  Matrix(const size_t rown, const size_t cols);                                     // Constructs an 'empty'  m * n matrix
+  Matrix(std::size_t rows, std::size_t cols, std::initializer_list<T> initializer); // Constructs an m * n matrix using an initializer list
+  // ** ADD CONSTRUCTOR FROM VEC OF VEC AND initializer_list of VEC
 
-  std::span<T> row_at();
+  // Public helpers
+  // need const/nonconst, also figure out required refs.
+  std::span<T> col_at(int j);                                                       // returns a span of a specific col
+  std::vector<T> row_at(size_t i);                                                  // returns a vector of a specific row
 
-
-  void print();
+  void print() const;                                                               // Prints rows
 };
 
 template <typename T>
-Matrix<T>::Matrix(int m, int n) : m(m), n(n), data(m * n) { }
+Matrix<T>::Matrix(size_t rows, size_t cols) : rows(rows), cols(cols), data(rows * cols) { }
 
 template <typename T>
-void Matrix<T>::print()
+Matrix<T>::Matrix(std::size_t rows, std::size_t cols, std::initializer_list<T> initializer) : rows(rows), cols(cols), data(initializer) { }
+
+template <typename T>
+std::span<T> Matrix<T>::col_at(int col)
 {
-  for (size_t i = 0; i < data.size(); ++i) {
-    std::cout << data[i] << ", ";
-    std::cout << std::endl;
+  return std::span(&data[col * rows], rows);
+}
+
+template <typename T>
+std::vector<T> Matrix<T>::row_at(size_t row)
+{
+  std::vector<T> vec;
+  vec.reserve(this->cols);
+  for (size_t col = 0; col < this->cols; ++col)
+  {
+    vec.push_back(this->data[col * this->rows + row]);
   }
+
+  return vec;
+}
+
+template <typename T>
+void Matrix<T>::print() const {
+    for (std::size_t r = 0; r < rows; ++r) {
+        for (std::size_t c = 0; c < cols; ++c) {
+            std::cout << data[c * rows + r];
+            if (c + 1 < cols) std::cout << ", ";
+        }
+        std::cout << '\n';
+    }
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 
 int main() {
 
-  Matrix<int> matrix = Matrix<int>(10, 10);
+  Matrix<int> matrix = Matrix<int>(2, 3, {0, 1, 2, 3, 4, 5});
   matrix.print();
 
+  std::vector<int> vec = matrix.row_at(1);
+  for (size_t i = 0; i < vec.size(); ++i)
+  {
+    std::cout << vec[i] << std::endl;
+  }
 }
